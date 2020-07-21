ee7a6b8694d0e58347734ec0f0156464f910be98
Aga Klyz <klyz.aga@gmail.com>
2020-07-21 16:12:30 +1000

    finished design and functionality

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -5,12 +5,14 @@ class QuestionsController < ApplicationController
 
   def answer
     @question = params[:question].capitalize
-    if @question == 'I am going to work'
+    if @question == 'I am going to work' || @question == "I'm going to work" || @question == 'Im going to work'
       @answer = 'Great!'
     elsif @question.include? '?'
-      @answer = 'silly question, get dressed and go to work!'
+      @answer = 'Silly question, get dressed and go to work!'
+    elsif @question.include? '!'
+      @answer = "Don't yell at me!!!"
     else
-      @answer = "i don't care, get dressed and go to work!"
+      @answer = "I don't care, get dressed and go to work!"
     end
   end
 end
